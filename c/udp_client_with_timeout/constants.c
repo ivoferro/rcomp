84c6c81a555046531f9232865b6578e9c3e9b8d2
ivoferro <ivoferro1995@gmail.com>
2017-04-11 23:30:20 +0100

    implements an UDP client w/ timeout [C][implementation]

---
+++ c/udp_client_with_timeout/constants.c
@@ -0,0 +1,15 @@
+/**
+ * constants.c
+ */
+
+const int TIMEOUT = 3;
+
+const int SERVER_ADDRESS_REQUIRED = -1;
+
+const int INVALID_SERVER_ADDRESS = -2;
+
+const int INVALID_LOCAL_ADDRESS = -3;
+
+const int OPEN_SOCKET_FAILED = -4;
+
+const int BIND_SOCKET_FAILED = -5;
